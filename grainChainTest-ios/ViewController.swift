403247a9aa190fb3259f891b306430e257b5bba0
Alfredo Fregoso <odrfla94@gmail.com>
2018-10-18 17:55:14 -0500

    Initial Commit

---
+++ grainChainTest-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  grainChainTest-ios
+//
+//  Created by Alfredo Fregoso  on 10/18/18.
+//  Copyright © 2018 Alfredo Fregoso . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
